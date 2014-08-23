971f114ee0f7a61607d7839751b9f2ed207feef5
RandellK02 <RandellK02@gmail.com>
2014-08-23 15:52:43 -0700

    adding globals

---
+++ Printer Migration/Globals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Printer_Migration
+{
+    public class Globals
+    {
+        public static List<byte> temp = new List<byte>();
+        public static List<String> users = new List<String>();
+        public static List<String> computers = new List<String>();
+        public static List<String> onlineComputers = new List<String>();
+        public static List<String> offlineComputers = new List<String>();
+    }
+}
