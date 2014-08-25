a1149ca61a5f04816fe116c1563b246b84c23cd7
RandellK02 <RandellK02@gmail.com>
2014-08-25 00:40:15 -0700

    Added Test Files and mindmap

---
+++ Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using HTMLReportEngine;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = @"C:\Users\Randell\";
+            string[] users = { "Randy", "Jacky", "Philip", "Reese", "Nick" };
+            string retiredPrinter = "08-colormfp-03";
+            bool successfulDelete = true, sAdd = false, offline = false, online = true;
+
+            DataSet ds = new DataSet("Summary");
+
+            DataTable dtUsers = new DataTable();
+            dtUsers.Columns.Add("User", typeof(string));
+            dtUsers.Columns.Add("retiredPrinter", typeof(string));
+            dtUsers.Columns.Add("successfulDelete", typeof(bool));
+            dtUsers.Columns.Add("sAdd", typeof(bool));
+            dtUsers.Columns.Add("offline", typeof(bool));
+            dtUsers.Columns.Add("Online", typeof(bool));
+
+            foreach (string user in users)
+            {
+                dtUsers.Rows.Add(user, retiredPrinter, successfulDelete, sAdd, offline, online);
+            }
+            dtUsers.Rows.Add(users[0], retiredPrinter, successfulDelete, sAdd, offline = true, online = false);
+            ds.Tables.Add(dtUsers);
+            ds.WriteXml(path + "source.xml");
+
+            DataSet copy = new DataSet("copy");
+            copy.ReadXml(path + "source.xml");
+            Report report = new Report();
+            report.ReportTitle = "Printer Migration Report";
+            report.ReportSource = copy;
+
+            Section release = new Section("Online", "Online: ");
+            Section id = new Section("User", "User: ");
+            release.SubSection = id;
+            report.Sections.Add(release);
+            report.ReportFields.Add(new Field("retiredPrinter", "Retired Printer", 12));
+            report.ReportFields.Add(new Field("successfulDelete", "Deletion Successful", 12));
+            report.ReportFields.Add(new Field("sAdd", "Add Successful", 12));
+            report.SaveReport(path + "Report.htm");
+        }
+    }
+}
