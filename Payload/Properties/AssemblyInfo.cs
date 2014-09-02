c9688682f0aee64268359bf7a18880539ae778e0
RandellK02 <RandellK02@gmail.com>
2014-09-01 21:43:34 -0700

    cont.

--- Payload/Properties/AssemblyInfo.cs
+++ Payload/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Payload")]
+[assembly: AssemblyTitle("CalRecycle_Printer_Upgrade")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Payload")]
+[assembly: AssemblyCompany("CalRecycle")]
+[assembly: AssemblyProduct("CalRecycle_Printer_Upgrade")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
